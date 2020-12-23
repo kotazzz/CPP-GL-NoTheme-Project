7bb8085d28845e3ee0d2691a97972bac3f5fa313
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-23 18:28:15 +0300

    Some Changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-using namespace std;
-#define RETURN return 0
+#include <list>
 
+using namespace std;
+
+
+int winsize_x = 100;
+int winsize_y = 70;
+list<list<char>> window_storage;
+
+void init(list<list<char>> strg, int x, int y){
+	list<char> tline;
+	tline.resize(x, '1');
+	for(int i; i < y; i++){
+		strg.push_back(tline);
+	}
+	
+}
 int main() {
-    cout << "Hello, world!" << endl;
+	cout << "Meow" << endl;
 	system ("pause");
-    RETURN;
+    return 0;
 }
